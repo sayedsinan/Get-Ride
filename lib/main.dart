24245145fd7063aa1a5319c946921a8f1ee6e160
Sayed Sinan Umer Jifry <sayedsinan7@gmail.com>
2025-08-09 19:48:29 +0530

    Inital Commit with project basic structure

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: ,
+    );
+  }
+}
