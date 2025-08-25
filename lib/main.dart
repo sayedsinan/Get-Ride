b4e8d77736eaf35a4efcba48922d75e983cd2202
Sayed Sinan Umer Jifry <sayedsinan7@gmail.com>
2025-08-25 18:43:50 +0530

    Minor update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get_ride/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: ,
+      home: LoginPage(),
     );
   }
 }
